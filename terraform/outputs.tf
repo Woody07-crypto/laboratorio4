5b6c4b463792f0207e6d2746d5b5abe262c7817a
SamArtz <99484116+SamArtz@users.noreply.github.com>
2026-05-30 09:40:55 -0600

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,7 +1,19 @@
+output "crear_cliente_lambda_arn" {
+  description = "ARN of the crear_cliente Lambda function"
+  value       = aws_lambda_function.crear_cliente.arn
+}
+
+output "realizar_transferencia_lambda_arn" {
+  description = "ARN of the realizar_transferencia Lambda function"
+  value       = aws_lambda_function.realizar_transferencia.arn
+}
+
 output "clientes_url" {
+
   value = "${aws_api_gateway_deployment.api_deployment.invoke_url}/clientes"
 }
 
 output "transferencias_url" {
+
   value = "${aws_api_gateway_deployment.api_deployment.invoke_url}/transferencias"
 }
